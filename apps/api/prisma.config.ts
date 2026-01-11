d4848166f2323e53f450fda1be199e12f53e50e4
Josh Horwitz <jahorwitz94@gmail.com>
2026-01-11 20:58:18 +0000

    fix: use url encoded password

--- apps/api/prisma.config.ts
+++ apps/api/prisma.config.ts
@@ -3,7 +3,6 @@ import type { PrismaConfig } from "prisma";
 import { env } from "prisma/config";
 
 console.log("Prisma config - DATABASE_URL:", env("DATABASE_URL"));
-console.log("Env vars: ", JSON.stringify(process.env, null, 2));
 
 export default {
   schema: "prisma/schema.prisma",
