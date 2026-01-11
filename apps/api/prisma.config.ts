7427d6950d9eeb725010dc1925d9cef2ccce8cb7
Josh Horwitz <jahorwitz94@gmail.com>
2026-01-11 20:15:22 +0000

    fix: create .env file during docker build

--- apps/api/prisma.config.ts
+++ apps/api/prisma.config.ts
@@ -2,6 +2,9 @@ import "dotenv/config";
 import type { PrismaConfig } from "prisma";
 import { env } from "prisma/config";
 
+console.log("Prisma config - DATABASE_URL:", env("DATABASE_URL"));
+console.log("Env vars: ", JSON.stringify(process.env, null, 2));
+
 export default {
   schema: "prisma/schema.prisma",
   migrations: {
